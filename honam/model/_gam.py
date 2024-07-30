9a299f732a3fd5f5a4507ce0465b118d9a18b4ca
Minkyu Kim <31180601+gim4855744@users.noreply.github.com>
2024-07-30 16:25:33 +0900

    add get_interactions()

--- honam/model/_gam.py
+++ honam/model/_gam.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import torch
 
 from torch.nn import Module, ModuleList, Linear, LeakyReLU
@@ -37,8 +39,9 @@ class HONAM(PyTorchModel):
     def __init__(self, n_features, order, task, ckpt_path):
         super().__init__(task, ckpt_path)
         self._order = order
+        self._emb_size = 32  # size of feature vector
         self._feature_nets = ModuleList([FeatureNet() for _ in range(n_features)])
-        self._output_layer = Linear(order * 32, 1)
+        self._output_layer = Linear(order * self._emb_size, 1)
 
     def forward(self, x):
         
@@ -65,3 +68,80 @@ class HONAM(PyTorchModel):
         x = self._output_layer(interactions)
 
         return x
+    
+    def interpret(self, x, *args):
+
+        """Get feature contributions from data samples.
+
+        Input
+        --------------------
+        - x: Batch of data samples. Shape (N, n_features).
+        - *args: feature ids.
+
+        Return
+        --------------------
+        - contribution of the feature specified by *args.
+
+        Example
+        --------------------
+        # get the contribution of 1st feature from x
+        > model.interpret(x, 0)
+
+        # get the contribution of 2nd-order feature from x
+        > model.interpret(x, 1, 3)  # contribution of cross feature (x1 * x3).
+        """
+
+        assert self._order >= len(args)
+
+        self.eval()
+
+        device = next(self.parameters()).device
+
+        x = torch.tensor(x, dtype=torch.float32, device=device)
+        x = x.T.unsqueeze(dim=2)
+        x = [feature_net(xi) for feature_net, xi in zip(self._feature_nets, x)]
+        x = torch.stack(x, dim=1)
+
+        x = x[:, args]
+        x = x.prod(dim=1)
+        weight = self._output_layer.weight.T[(len(args) - 1) * self._emb_size: len(args) * self._emb_size]
+
+        contributions = x @ weight
+
+        return contributions.detach().cpu().numpy()
+    
+    def get_interactions(self, x):
+
+        """Get all feature contributions.
+
+        Input
+        --------------------
+        - x: Batch of data samples. Shape (N, n_features).
+
+        Return
+        --------------------
+        - contributions of features.
+        - feature names
+        """
+
+        n_features = x.shape[1]
+        contributions = []
+        names = []
+
+        def _get_interactions(start_idx, items=[]):
+
+            if len(items) > self._order:
+                return
+            
+            contributions.append(self.interpret(x, *items))
+            names.append(items)
+
+            for i in range(start_idx, n_features):
+                _get_interactions(i + 1, items + [i])
+
+        for i in range(n_features):
+            _get_interactions(start_idx=i + 1, items=[i])
+
+        contributions = np.concatenate(contributions, axis=1)
+        
+        return contributions, names
--- run.py
+++ run.py
@@ -12,7 +12,7 @@ from honam.utils import set_global_seed, evaluate
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, choices=DATASETS.keys(), required=True, help='dataset name')
 parser.add_argument('--seed', type=int, required=True, help='random seed, 0<=seed<=4')
-parser.add_argument('--mode', type=str, required=True, choices=['train', 'test'], help='train or test')
+parser.add_argument('--mode', type=str, required=True, choices=['train', 'test', 'interpret'], help='train, test or interpret')
 parser.add_argument('--order', default=2, type=int, help='interaction order, only used for HONAM, HONAMCrossNet, and CrossNet')
 parser.add_argument('--num_workers', default=0, type=int, help='number of workers of dataloader')
 parser.add_argument('--device', default='cpu', type=str, choices=['cpu', 'cuda', 'mps'], help='device name')
@@ -47,12 +47,17 @@ def main():
         hparam = {'n_features': n_features, 'order': args.order, 'task': task, 'ckpt_path': ckpt_path}
         model = HONAM(**hparam).to(args.device)
         model.fit(x_train, y_train, x_val, y_val, num_workers=args.num_workers)
-    else:
+    elif args.mode == 'test':
         checkpoint= torch.load(ckpt_path)
         model = checkpoint['model'].to(args.device)
         model.load_state_dict(checkpoint['state_dict'])
         test_y_hat = model.predict(x_test)
         evaluate(y_test, test_y_hat, task)
+    else:
+        checkpoint= torch.load(ckpt_path)
+        model = checkpoint['model'].to(args.device)
+        model.load_state_dict(checkpoint['state_dict'])
+        model.get_interactions(x_test)
 
 
 if __name__ == '__main__':
